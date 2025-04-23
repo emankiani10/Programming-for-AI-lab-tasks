2b58245580773ad360edb0d45d8cc89c69f0fd3c
emankainif23@nutech.edu.pk <your_email@example.com>
2025-04-23 10:16:56 +0500

    Save current work before pulling

---
+++ New R script.R
@@ -0,0 +1,8 @@
+data(mtcars)
+str(mtcars)
+head(mtcars)
+View(mtcars)
+
+setwd("C:/Users/Eman Asghar/Documents/Programming-for-AI-lab-tasks")
+
+getwd()
---
+++ PFAI lab 4 task1.R
@@ -0,0 +1,2 @@
+print("This is source file.")
+a = 2
